05426ed3ffef64f582ada5cbbca0dc321950a2c7
priyanka <priyankachadar1105@gmail.com>
2021-12-11 20:22:20 +0530

    make it better

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const adminRoute =require("./routes/admin");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 dotenv.config();
@@ -26,9 +24,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/admins",adminRoute);
 app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
